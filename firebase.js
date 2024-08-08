a8104c377a64d4fd4136da49e2ddc1e918438003
Rohan Ahuja <rohanahuja@Rohans-MacBook-Pro-2.local>
2024-08-07 23:43:31 -0400

    Added upload route

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,6 +19,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
 const firestore = getFirestore(app);
 
-export { firestore }
+export { storage, firestore }
